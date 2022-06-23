5a0e5c07ea4f135f7cc93b73c9f9b244744605a5
Sebas <sebas.654@gmail.com>
2022-06-23 18:37:09 -0300

    agrego un loader

---
+++ MercadoLibreApp/static/js/estilos.js
@@ -0,0 +1,11 @@
+$(window).load(function() {
+    $(".loader").fadeOut("slow");
+});
+
+$("#btn-buscar").click(function () {
+    $(".loader").fadeIn("slow")
+});
+
+$("#button-addon2").click(function (){
+    $(".loader").fadeIn("slow")
+});
